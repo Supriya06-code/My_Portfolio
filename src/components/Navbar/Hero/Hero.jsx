a3355e10125bb564efe08a2f25dfd85f9983195b
Supriya06-code <supriyanjadhav2000@gmail.com>
2024-08-10 16:23:58 +0530

    first commit

---
+++ src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { getImageUrl } from '../../../utils';
+import styles from "./Hero.module.css";
+const Hero = () => {
+  return (
+   <section className={styles.container}>
+    <div className={styles.content}> 
+    <h1 className={styles.title}>Hii,I'm Supriya</h1></div>
+    <p className={styles.description}>I'm a passionate and eager software engineer with hands-on experience in Web-Dev. Excited to bring my fresh perspectives and technical skills to the industry. Let's connect to discuss opportunities!</p>
+    <a href="mailto:supriyanjadhav2000@gmail.com" className={styles.contactBtn}>Contact Me</a>
+    <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg}/>
+    <div className={styles.topBlur}></div>
+    <div className={styles.bottomBlur}></div>
+   </section>
+  );
+};
+
+export default Hero
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
+  plugins: [react()],
+  css:{
+    modules:{
+      localsConvention: "camelCase"
+    }
+  }
+  
+});
